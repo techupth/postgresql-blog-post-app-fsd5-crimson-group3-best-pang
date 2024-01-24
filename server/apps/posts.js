2817601d008861587c8a89fac069633d118352f9
pang <pangton28.p@gmail.com>
2024-01-24 23:37:11 +0700

    feat(exercise): add CRUD by SQL

--- server/apps/posts.js
+++ server/apps/posts.js
@@ -1,23 +1,80 @@
 import { Router } from "express";
+import { connectionPool } from "../utils/db.js";
 
 const postRouter = Router();
 
 postRouter.get("/", async (req, res) => {
-  const status = req.query.status;
-  const keywords = req.query.keywords;
-  const page = req.query.page;
+  try {
+    const status = req.query.status || "";
+  const keywords = req.query.keywords || "";
+  const page = req.query.page || 1;
+
+  const PAGE_SIZE = 5;
+  const offset = (page - 1) * PAGE_SIZE;
+
+  let query = "";
+  let values = [];
+
+	if (status && keywords) {
+    query = `select * from posts
+    where status=$1
+    and title ilike $2
+    limit $3
+    offset $4`;
+    values = [status, keywords, PAGE_SIZE, offset];
+  } else if (keywords) {
+    query = `select * from posts
+    where title ilike $1
+    limit $2
+    offset $3`;
+    values = [keywords, PAGE_SIZE, offset];
+  } else if (status) {
+    query = `select * from posts
+    where status=$1
+    limit $2
+    offset $3`;
+    values = [status, PAGE_SIZE, offset];
+  } else {
+    query = `select * from posts
+    limit $1
+    offset $2`;
+    values = [PAGE_SIZE, offset];
+  }
+
+  const results = await connectionPool.query(query, values);
+
+  let totalResultCount = 0;
+
+    if (results.rows.length != 0) {
+      totalResultCount = results.rows[0].count;
+    }
 
   return res.json({
-    data: [],
+    data: results.rows,
+    total_pages: Math.ceil(totalResultCount / PAGE_SIZE),
   });
+  } catch (error) {
+    return res.json({
+      message: Error`${error}`,
+    });
+  }
 });
 
 postRouter.get("/:id", async (req, res) => {
   const postId = req.params.id;
+  try {
+    const result = await connectionPool.query("select * from posts where post_id=$1", 
+    [ postId,]
+    );
 
-  return res.json({
-    data: {},
-  });
+    return res.json({
+      data: result.rows[0],
+    });
+  } catch (error) {
+    return res.json({
+      message: Error`${error}`,
+    });
+  }
 });
 
 postRouter.post("/", async (req, res) => {
@@ -29,9 +86,30 @@ postRouter.post("/", async (req, res) => {
     published_at: hasPublished ? new Date() : null,
   };
 
-  return res.json({
-    message: "Post has been created.",
-  });
+  try {
+    await connectionPool.query(
+      `insert into posts(title,content,status,category,created_at,updated_at,published_at)
+    values ($1, $2, $3, $4, $5, $6, $7)`,
+      [
+        newPost.title,
+        newPost.content,
+        newPost.status,
+        newPost.category,
+        newPost.created_at,
+        newPost.updated_at,
+        newPost.published_at,
+      ]
+    );
+
+    return res.json({
+      message: "Post has been created.",
+    });
+  } catch (error) {
+    return res.json({
+      message: Error`${error}`,
+    });
+  }
+  
 });
 
 postRouter.put("/:id", async (req, res) => {
@@ -43,18 +121,44 @@ postRouter.put("/:id", async (req, res) => {
     published_at: hasPublished ? new Date() : null,
   };
   const postId = req.params.id;
+  try {
+    await connectionPool.query(
+      `UPDATE posts 
+    SET title=$1,content=$2,status=$3,updated_at=$4,published_at=$5 
+    WHERE post_id=$6`,
+      [
+        updatedPost.title,
+        updatedPost.content,
+        updatedPost.status,
+        updatedPost.updated_at,
+        updatedPost.published_at,
+        postId,
+      ]
+    );
 
-  return res.json({
-    message: `Post ${postId} has been updated.`,
-  });
+    return res.json({
+      message: `Post ${postId} has been updated.`,
+    });
+  } catch (error) {
+    return res.json({
+      message: Error`${error}`,
+    });
+  }
 });
 
 postRouter.delete("/:id", async (req, res) => {
   const postId = req.params.id;
+  try {
+    await connectionPool.query(`delete from posts where post_id=$1`, [postId]);
 
-  return res.json({
-    message: `Post ${postId} has been deleted.`,
-  });
+    return res.json({
+      message: `Post ${postId} has been deleted.`,
+    });
+  } catch (error) {
+    return res.json({
+      message: Error`${error}`,
+    });
+  }
 });
 
 export default postRouter;
